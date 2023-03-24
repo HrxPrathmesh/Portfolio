a64be37b5d7841208cf2af973bc0a722625fd84b
Hrx Prathmesh <hrxprathmesh@gmail.com>
2023-03-24 17:48:25 +0530

    Done

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Prathmesh.
+      <time>{YEAR}</time> © Hrx Prathmesh.
      <a href="https://github.com/hrxprathmesh/">Source Code</a>
       <style jsx>{`
         a {
